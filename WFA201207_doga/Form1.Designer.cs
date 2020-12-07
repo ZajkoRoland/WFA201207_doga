6377f408a9af6f1bb54094b5064c716a206eb6e0
Zajkó Roland <zajko.roland@verebelyszki.hu>
2020-12-07 21:06:59 +0100

    Add project files.

---
+++ WFA201207_doga/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace WFA201207_doga
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LinkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            this.RichTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Button3 = new System.Windows.Forms.Button();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.ComboBox2 = new System.Windows.Forms.ComboBox();
+            this.ComboBox1 = new System.Windows.Forms.ComboBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LinkLabel1
+            // 
+            this.LinkLabel1.AutoSize = true;
+            this.LinkLabel1.Location = new System.Drawing.Point(497, 319);
+            this.LinkLabel1.Name = "LinkLabel1";
+            this.LinkLabel1.Size = new System.Drawing.Size(37, 13);
+            this.LinkLabel1.TabIndex = 29;
+            this.LinkLabel1.TabStop = true;
+            this.LinkLabel1.Text = "NOPE";
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(419, 319);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(66, 13);
+            this.Label4.TabIndex = 28;
+            this.Label4.Text = "URL (if any):";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(228, 18);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(36, 13);
+            this.Label3.TabIndex = 27;
+            this.Label3.Text = "Album";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(79, 18);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(30, 13);
+            this.Label2.TabIndex = 26;
+            this.Label2.Text = "Artist";
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(79, 121);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(273, 20);
+            this.TextBox1.TabIndex = 25;
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.Location = new System.Drawing.Point(79, 158);
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.Size = new System.Drawing.Size(282, 275);
+            this.DataGridView1.TabIndex = 24;
+            // 
+            // RichTextBox1
+            // 
+            this.RichTextBox1.Location = new System.Drawing.Point(500, 158);
+            this.RichTextBox1.Name = "RichTextBox1";
+            this.RichTextBox1.Size = new System.Drawing.Size(222, 110);
+            this.RichTextBox1.TabIndex = 23;
+            this.RichTextBox1.Text = "";
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Location = new System.Drawing.Point(385, 158);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(94, 110);
+            this.PictureBox1.TabIndex = 22;
+            this.PictureBox1.TabStop = false;
+            // 
+            // Button3
+            // 
+            this.Button3.Location = new System.Drawing.Point(614, 381);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(108, 52);
+            this.Button3.TabIndex = 21;
+            this.Button3.Text = "Edit Selected";
+            this.Button3.UseVisualStyleBackColor = true;
+            // 
+            // Button2
+            // 
+            this.Button2.Location = new System.Drawing.Point(500, 381);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(108, 52);
+            this.Button2.TabIndex = 20;
+            this.Button2.Text = "Add URL";
+            this.Button2.UseVisualStyleBackColor = true;
+            // 
+            // ComboBox2
+            // 
+            this.ComboBox2.FormattingEnabled = true;
+            this.ComboBox2.Location = new System.Drawing.Point(231, 50);
+            this.ComboBox2.Name = "ComboBox2";
+            this.ComboBox2.Size = new System.Drawing.Size(121, 21);
+            this.ComboBox2.TabIndex = 19;
+            // 
+            // ComboBox1
+            // 
+            this.ComboBox1.FormattingEnabled = true;
+            this.ComboBox1.Location = new System.Drawing.Point(79, 50);
+            this.ComboBox1.Name = "ComboBox1";
+            this.ComboBox1.Size = new System.Drawing.Size(121, 21);
+            this.ComboBox1.TabIndex = 18;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(79, 93);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(105, 13);
+            this.Label1.TabIndex = 17;
+            this.Label1.Text = "Search in track\'s title";
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(385, 381);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(108, 52);
+            this.Button1.TabIndex = 16;
+            this.Button1.Text = "Add Discography";
+            this.Button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LinkLabel1);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.DataGridView1);
+            this.Controls.Add(this.RichTextBox1);
+            this.Controls.Add(this.PictureBox1);
+            this.Controls.Add(this.Button3);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.ComboBox2);
+            this.Controls.Add(this.ComboBox1);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.Button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.LinkLabel LinkLabel1;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.TextBox TextBox1;
+        internal System.Windows.Forms.DataGridView DataGridView1;
+        internal System.Windows.Forms.RichTextBox RichTextBox1;
+        internal System.Windows.Forms.PictureBox PictureBox1;
+        internal System.Windows.Forms.Button Button3;
+        internal System.Windows.Forms.Button Button2;
+        internal System.Windows.Forms.ComboBox ComboBox2;
+        internal System.Windows.Forms.ComboBox ComboBox1;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Button Button1;
+    }
+}
+
---
+++ WFA201207_doga/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA201207_doga
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        public Form1()
+        {
+            conn = new SqlConnection(
+               @"Server=(localdb)\MSSQLLocalDB;" +
+               @"AttachDbFileName=|DataDirectory|\Resources\music.mdf;");
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(@"pendulum.txt");
+            string s = sr.ReadLine();
+            string[] darabol = s.Split(';');
+        }
+    }
+}
